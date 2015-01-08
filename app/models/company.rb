8a8812b9cd82bdf64def574363883cca1d719458
Matthew K <mattjk@outlook.com>
2015-01-07 19:34:06 -0500

    Added secure password, and validating email

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,5 +1,8 @@
 class Company < ActiveRecord::Base
   validates :name, presence:true, uniqueness:true, length: {maximum:25, minimum:5}
+  validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+  
+  has_secure_password
   
   def updateStats
     self.update(name: "[deleted]")
