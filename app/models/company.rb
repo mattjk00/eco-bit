9bc1e71dc34a74b98fc43e0fadacdeccbfcf02b1
Matthew K <mattjk@outlook.com>
2015-01-07 19:56:47 -0500

    actually added validation to password

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,6 +1,7 @@
 class Company < ActiveRecord::Base
   validates :name, presence:true, uniqueness:true, length: {maximum:25, minimum:5}
   validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+  validates :password, :confirmation => true, length { minimum:7, maximum:36 }
   
   has_secure_password
   
