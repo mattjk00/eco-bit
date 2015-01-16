1ea1f27c3c6fea5ece8d0024fbde80d474a66da3
Matthew K <mattjk@outlook.com>
2015-01-15 21:42:41 -0500

    more ui changes

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,25 +1,35 @@
 $(document).ready(function() {
 	
 	var sidebar = $("#sidebar"); // get the sidebar
+	var sidebarContents = $("#sidebar_contents");
 	var sidebarToggle = $("#sidebar_toggle"); // get the toggle button
 	var sidebarHidden = true;
 	
 	sidebar.hide();
+	sidebarContents.hide();
+	sidebarContents.animate({opacity:"0.0"}, 0);
 	
 	sidebarToggle.mouseover(function(){
-		sidebarToggle.stop().animate({width:"14px"}, 200);
+		sidebarToggle.stop().animate({opacity:"0.7"}, 200);
 	});
 	sidebarToggle.mouseout(function(){
-		sidebarToggle.stop().animate({width:"10px"}, 200);
+		sidebarToggle.stop().animate({opacity:"1.0"}, 200);
 	});
 	
 	sidebarToggle.click(function(){
 		if (sidebarHidden) {
-			sidebar.show();
+			sidebar.show(function(){
+				sidebar.animate({width:"25%"}, 0, function(){
+					sidebarContents.show(function(){
+						sidebarContents.animate({opacity:"1.0"}, 10);
+					});
+				});
+			});
 			sidebarHidden = false;
 		}
 		else {
-			sidebar.hide();	
+			sidebar.hide();
+			sidebarContents.hide();
 			sidebarHidden = true;
 		}
 	});
